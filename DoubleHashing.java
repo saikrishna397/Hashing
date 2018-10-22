f91d29c5e2afbbb45b7a17c41b79244399f6f5ea
Sai Krishna Reddy Syamala <saikrishnareddy.gcs@gmail.com>
2018-10-21 20:50:08 -0500

    Changed the code

--- DoubleHashing.java
+++ DoubleHashing.java
@@ -1,37 +1,43 @@
-
-//package sxs169430;
-
-import java.util.HashSet;
+/**
+ *  Implementation of Double Hashing in Java
+ *  @author Sai Krishna Reddy Syamala (sxs169430)
+ *  @author Mihir Hindocha (mxh170027)
+ *  Ver 1.0: 10/21/2018
+ */
+package sxs169430;
 
 public class DoubleHashing<T> {
 	private T[] table; // The array of elements
 	private int size; // Current size
-	private int TABLE_SIZE;
 	private int PRIME;
-
+	private static final int DEFAULT_SIZE = 1000; // default array size if not provided
 	/* Constructor */
+	public DoubleHashing() {
+		this(DEFAULT_SIZE);
+	}
+	
 	public DoubleHashing(int ts) {
-		TABLE_SIZE = ts;
-		formNewArray(ts);
+		formArray(ts);
 		PRIME = getPrime(ts);
 		clear();
 	}
-
+	/**
+	 * creates a new array with the specified size
+	 * @param arraySize
+	 */
 	@SuppressWarnings("unchecked")
-	private void formNewArray(int arraySize) {
+	private void formArray(int arraySize) {
 		table = (T[]) new Object[getPrime(arraySize)];
 	}
 
 	/* Function to get prime number less than table size for myhash2 function */
 	public int getPrime(int n) {
-//		n = TABLE_SIZE; /** is it required as we are passing ts to the function above anyways */
-		while (!isPrime(--n))
-			; 
+		while (!isPrime(--n)); 
 
-		return n;/* Return a prime number */
+		return n;//Returns a prime number less than the table size
 	}
 
-	/* Check if the numer is a prime number */
+	/* Check if the number is a prime number */
 	protected static boolean isPrime(int m) {
 		int n = m;
 		for (int i = 2; i * i <= n; i++) {
@@ -42,34 +48,49 @@ public class DoubleHashing<T> {
 		return true;
 	}
 
-	// find - returns the entry
-	// contains
-
-	/* Function to get number of key-value pairs */
+	/* Function to get number of elements in the table */
 	public int size() {
 		return size;
 	}
-
+	// returns true if the table is empty
 	public boolean isEmpty() {
 		return size == 0;
 	}
-
+	/**
+	 * returns a hashValue for the element to be inserted
+	 * @param x - element to be placed in the table
+	 * @return hashValue
+	 */
+	private int myhash1(T x) {
+		int hashVal = Math.abs(x.hashCode());
+		hashVal %= table.length;
+		if (hashVal < 0)
+			hashVal += table.length;
+		return hashVal;
+	}
+	/**
+	 * second hash function to reduce the collision
+	 * @param x - element to be inserted
+	 * @return hashValue
+	 */
+	private int myhash2(T x) {
+		int hashVal = Math.abs(x.hashCode());
+		hashVal %= PRIME;
+		if(hashVal < 0)
+			hashVal += table.length;
+		return PRIME - hashVal%PRIME;
+	}	
 	/* Function to clear hash table */
 	public void clear() {
 		size = 0;
 		for (int i = 0; i < table.length; i++)
 			table[i] = null;
 	}
-
-	/* Function to get value of a key */
-	public T get(T key) {
-		int hash1 = find(key);
-		if (table[hash1] != null)
-			return table[hash1];
-		else
-			return null;
-	}
-
+	/**
+	 * returns a slot in the table for the given element
+	 * @param key - element to be inserted
+	 * @return - index in the table
+	 */
 	public int find(T key) {
 		// returns the entry for the key
 		int hash1 = myhash1(key);
@@ -82,12 +103,16 @@ public class DoubleHashing<T> {
 		return hash1; 
 	}
 
-	/* Function to add a key*/
+	/**
+	 * function to add an element to the table
+	 * @param x - element to be added
+	 * @return - true if element is inserted, false if element already exists
+	 */
 	public boolean add(T x) {
 		// Insert x
 		int hash1 = find(x); 
-		if (table[hash1] == x)
-			return false;
+		if (contains(x))
+			return false; // if the element already exists in the table
 		table[hash1] = x;
 		size++;
 		// Rehash if the table is half full
@@ -98,25 +123,23 @@ public class DoubleHashing<T> {
 	}
 
 	/**
-	 * Expand the array and rehash all the old values.
+	 * function to expand the array and rehash all the old values.
 	 */
 	private void rehash() {
 		T[] oldArray = table;
-		/**
-		 * If we take newsize = 2* old size then it doesn't remain prime anymore maybe
-		 * that is the case. We need to find the next big prime number if we want to
-		 * reduce collisions
-		 */
 		// Create a new double-sized, empty table
-		formNewArray(2 * oldArray.length);
+		formArray(2 * oldArray.length);
 		size = 0;
-
 		// Copy the old array
 		for (T item : oldArray)
 			if (item != null)
 				add(item);
 	}
-
+	/**
+	 * function to check if the table contains the given element
+	 * @param key - element to be checked
+	 * @return - true if the element exists in the table.
+	 */
 	public boolean contains(T key) {
 		int hash1 = find(key);
 		if (table[hash1] != null)
@@ -124,7 +147,11 @@ public class DoubleHashing<T> {
 		return false;
 	}
 
-	/* Function to remove a key */
+	/**
+	 * Function to remove a key
+	 * @param key - element to be removed from the table
+	 * @return - true if element is removed  
+	 */
 	public boolean remove(T key) {
 		int hash1 = find(key);
 		if (table[hash1] != null) {
@@ -134,21 +161,4 @@ public class DoubleHashing<T> {
 		} else
 			return false;
 	}
-
-	private int myhash1(T x) {
-		int hashVal = Math.abs(x.hashCode());
-		hashVal %= table.length;
-		if (hashVal < 0)
-			hashVal += table.length;
-		return hashVal;
-	}
-
-	private int myhash2(T x) {
-		int hashVal = Math.abs(x.hashCode());
-		hashVal %= PRIME;
-		hashVal = PRIME - hashVal;
-		return hashVal;
-	}
-
-	
 }
