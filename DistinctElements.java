090293d54b7a9ae75219248c00bf7fb63ac8e00c
Sai Krishna Reddy Syamala <saikrishnareddy.gcs@gmail.com>
2018-10-21 20:50:48 -0500

    Added Comments

--- DistinctElements.java
+++ DistinctElements.java
@@ -1,8 +1,14 @@
+/**
+ *  Implementation of Distinct Elements class
+ *  @author Sai Krishna Reddy Syamala (sxs169430)
+ *  @author Mihir Hindocha (mxh170027)
+ *  Ver 1.0: 10/21/2018
+ */
+package sxs169430;
 
 public class DistinctElements {
-
 	static<T> int distinctElements(T[ ] arr) { 
-		DoubleHashing dh = new DoubleHashing(100);
+		DoubleHashing<T> dh = new DoubleHashing<>(100);
 		for(T i : arr) {
 			dh.add(i);
 		}
